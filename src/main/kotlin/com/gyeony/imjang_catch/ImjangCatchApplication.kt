53bc0f538121f4837d7d8b1d54fb31e7f0a34fba
Gyeony95 <rlg1133@naver.com>
2024-03-28 00:35:23 +0900

    init

---
+++ src/main/kotlin/com/gyeony/imjang_catch/ImjangCatchApplication.kt
@@ -0,0 +1,11 @@
+package com.gyeony.imjang_catch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImjangCatchApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImjangCatchApplication>(*args)
+}
---
+++ src/test/kotlin/com/gyeony/imjang_catch/ImjangCatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gyeony.imjang_catch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImjangCatchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
